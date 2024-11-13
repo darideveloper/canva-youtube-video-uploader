dd61ddee399ad942f6a63ca3621d19e25cb0f93f
generic-projects <abelsotovaldez@gmail.com>
2024-11-13 13:42:12 -0600

    feat(scraper): download canva video

--- __main__.py
+++ __main__.py
@@ -1,17 +1,23 @@
 import os
 from dotenv import load_dotenv
 from libs.scraper import Scraper
-
+from libs.xlsx import SpreadsheetManager
 
 load_dotenv()
-VAR = os.getenv("VAR")
-
+EXCEL_FILE = os.getenv("EXCEL_ROUTE")
+EXCEL_SHEET = os.getenv("EXCEL_SHEET")
+DOWNLOAD_FOLDER = os.getenv("DOWNLOAD_FOLDER")
 
 def main():
-    scraper = Scraper()
-    canva_video_path = scraper.download_canva_video("sample video 1")
-    scraper.upload_youtube_video(canva_video_path)
-
+    excel_reader = SpreadsheetManager(EXCEL_FILE)
+    excel_reader.set_sheet(EXCEL_SHEET)
+    data = excel_reader.get_data()
+    #print(data)
+    for row in range(1,len(data)):   
+        scraper = Scraper()
+        canva_video_path = scraper.download_canva_video(data[row][0], DOWNLOAD_FOLDER)
+        scraper.upload_youtube_video(canva_video_path)
+    
 
 if __name__ == "__main__":
     main()
--- libs/scraper.py
+++ libs/scraper.py
@@ -1,5 +1,6 @@
 from libs.chrome_dev import ChromDevWrapper
-
+import os
+from time import sleep
 
 class Scraper(ChromDevWrapper):
     
@@ -10,18 +11,39 @@ class Scraper(ChromDevWrapper):
         self.youtube_page = 'https://studio.youtube.com/'
         self.canva_page = 'https://www.canva.com/projects/'
     
-    def download_canva_video(self, video_name: str) -> str:
+    def download_canva_video(self, video_name: str, dowload_path) -> str:
         """ Download video from canva
         
         Args:
             video_name (str): name of video to download
+            download_path (str): path where the path will be downloaded
 
         Returns:
             str: path of downloaded video
         """
         
-        self.set_page(self.canva_page)
-        input("continue?")
+        print(video_name)
+        self.set_page(video_name)
+
+        mp4_files = [f for f in os.listdir(dowload_path) if f.endswith('.mp4')]
+
+        print(mp4_files)
+
+        self.click("._38oWvQ")
+
+        original_mp4_files = len(mp4_files)
+        original_files = mp4_files
+
+        #print(original_mp4_files)
+        while len(mp4_files) <= original_mp4_files:
+            mp4_files = [f for f in os.listdir(dowload_path) if f.endswith('.mp4')]    
+            #print(len(mp4_files))
+        
+        new_file = list(set(original_files) ^ set(mp4_files))[0]
+        new_file_path = dowload_path+"\\"+new_file
+        print(new_file_path)
+
+        return new_file_path
     
     def upload_youtube_video(self, video_path: str):
         """ Upload video to youtube
@@ -30,4 +52,8 @@ class Scraper(ChromDevWrapper):
             video_path (str): path of video to upload
         """
         self.set_page(self.youtube_page)
+        self.click('.yt-spec-touch-feedback-shape.yt-spec-touch-feedback-shape--touch-response-inverse')
+        #self.click('#select-files-button .yt-spec-touch-feedback-shape__stroke')
+        sleep(5)
+        self.send_data("input[type='file']", video_path)
         input("continue?")
