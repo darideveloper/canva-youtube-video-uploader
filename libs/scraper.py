855f8a0f07b3361761814fb8c4e9134038b6e0ba
generic-projects <abelsotovaldez@gmail.com>
2024-11-14 16:23:50 -0600

    feat(scraper): Upload canva video

--- libs/scraper.py
+++ libs/scraper.py
@@ -1,6 +1,6 @@
 import os
 from libs.chrome_dev import ChromDevWrapper
-
+from time import sleep
 
 class Scraper(ChromDevWrapper):
 
@@ -10,7 +10,7 @@ class Scraper(ChromDevWrapper):
         super().__init__(start_killing=True)
         self.youtube_page = 'https://studio.youtube.com/'
         self.canva_page = 'https://www.canva.com/projects/'
-
+    
     def download_canva_video(self, video_name: str, dowload_path) -> str:
         """ Download video from canva
 
@@ -21,12 +21,18 @@ class Scraper(ChromDevWrapper):
         Returns:
             str: path of downloaded video
         """
+        selectors = {
+            "downoad_btn": "._38oWvQ"
+        }
+
+        sleep(5)
 
         self.set_page(video_name)
-
+        
         mp4_files = [f for f in os.listdir(dowload_path) if f.endswith('.mp4')]
 
-        self.click("._38oWvQ")
+        self.wait_load(selectors["downoad_btn"])
+        self.click(selectors["downoad_btn"])
 
         original_mp4_files = len(mp4_files)
         original_files = mp4_files
@@ -54,6 +60,10 @@ class Scraper(ChromDevWrapper):
             "input_video": "input[type='file']",
             "preview_play_btn": '[icon="av:play-arrow"]',
             "video_title": "#textbox",
+            "kids_radio": "tp-yt-paper-radio-group tp-yt-paper-radio-button:nth-child(2)",
+            "next_btn": ".button-area.metadata-fade-in-section.style-scope.ytcp-uploads-dialog .yt-spec-touch-feedback-shape.yt-spec-touch-feedback-shape--touch-response-inverse",
+            "public_radio": "tp-yt-paper-radio-button[name='PUBLIC'] #radioContainer",
+            "save_btn": "#done-button"
         }
 
         # Open upload page
@@ -70,8 +80,26 @@ class Scraper(ChromDevWrapper):
         self.wait_load(selectors["preview_play_btn"], max_wait=300)
         
         # Write video data
-        print("\t\tWriting video data...")
-        self.set_innerhtml(selectors["video_title"], "Sample Video 1")
+        print("\t\tDetails...")
+        self.click(selectors["kids_radio"])
         
+        sleep(2)
+        self.click(selectors["next_btn"])    
+
+        print("\t\tVideo elements...")
+        sleep(2)
+        self.click(selectors["next_btn"])    
+
+        print("\t\tVerification...")
+        sleep(2)
+        self.click(selectors["next_btn"])
+        
+        print("\t\tVisibility...")
+        sleep(2)
+        self.click(selectors["public_radio"])
+
+        sleep(2)
+        self.click(selectors["save_btn"])
+        print("\t\t\tVideo uploaded...")
         input("Press enter to continue...")
         
